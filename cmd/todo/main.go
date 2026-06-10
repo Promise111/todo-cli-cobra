65038494b3d1f2fa50cd8c0699a90d55f911b5a9
Promise <promiseihunna@gmail.com>
2026-06-10 16:07:45 +0100

    Clean up main.go by removing commented-out code for improved readability and clarity in the Todo CLI application.

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -12,17 +12,9 @@ func main() {
 	log.SetPrefix("Todo CLI with cobra: ")
 	log.SetFlags(1)
 
-	// initiate cobra on startup
-	// var todos = commands.Todos
 	// New Storage
 	storage := internal.NewStorage[internal.Todos]("todo.json")
 	storage.Load(&commands.Todos)
-	// todos.Add("Buy bread")
-	// todos.Add("Bake some meat pies")
-	// todos.Edit(1,"Sky dive later this morning")
-	// todos.Toggle(1)
-	// todos.List()
-	// fmt.Printf("%+v \n\n", todos)
 	storage.Save(commands.Todos)
 	commands.Execute()
 }

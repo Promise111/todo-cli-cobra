db7734e601bc3e058f75b5511719baa7fbfbba7a
Promise <promiseihunna@gmail.com>
2026-06-10 16:15:01 +0100

    Refactor main.go to enhance clarity and remove commented-out code
    - Removed unnecessary commented-out code in main.go to improve readability.
    - Streamlined the structure for better understanding of the Todo CLI application.

---
+++ internal/commands/editCmd.go
@@ -0,0 +1,40 @@
+package commands
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+var editCmd = cobra.Command{
+	Use: "edit",
+	Aliases: []string{"enter"},
+	Short: "Edit a todo",
+	Long: "Edit a todo by index and title",
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string){
+		splitArgs := strings.SplitN(args[0], ":", 2)
+		if len(splitArgs) != 2 {
+			fmt.Println("Error: Invalid input format. Use 'add:title'")
+			os.Exit(1)
+		}
+		index, err := strconv.Atoi(splitArgs[0])
+		if err != nil {
+			fmt.Println("Error: Invalid index. Use a number.")
+			os.Exit(1)
+		}
+		if splitArgs[1] == "" {
+			fmt.Println("Error: Title is required.")
+			os.Exit(1)
+		}
+		Todos.Edit(index, splitArgs[1])
+		fmt.Printf("Task added: %s", args[0])
+	},
+}
+
+func init () {
+	editCmd.AddCommand(&editCmd)
+}
